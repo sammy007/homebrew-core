9b495c3d69ee646ecb37fca081abb4a6a80da475
Sammy Libre <7374093+sammy007@users.noreply.github.com>
2018-03-31 11:25:34 +0500

    Update monero to latest HF v0.12.0.0
    Git required because there is a submodule.

--- Formula/monero.rb
+++ Formula/monero.rb
@@ -1,8 +1,7 @@
 class Monero < Formula
-  desc "Official monero wallet and cpu miner"
-  homepage "https://getmonero.org/"
-  url "https://github.com/monero-project/monero/archive/v0.11.1.0.tar.gz"
-  sha256 "b5b48d3e5317c599e1499278580e9a6ba3afc3536f4064fcf7b20840066a509b"
+  desc "Monero: the secure, private, untraceable cryptocurrency"
+  homepage "https://getmonero.org"
+  url "https://github.com/monero-project/monero.git", :tag => "v0.12.0.0"
 
   bottle do
     sha256 "4605768a865c17daae09b3e1ddfd96babe0779126ff1ed90db26238e020d8283" => :high_sierra
